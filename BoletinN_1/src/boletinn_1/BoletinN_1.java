74a8681698bd3c41c22fb84447921381721dfcb5
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-10-27 09:27:31 +0200

    Inicio P1

---
+++ BoletinN_1/src/boletinn_1/BoletinN_1.java
@@ -0,0 +1,15 @@
+
+package boletinn_1;
+
+
+public class BoletinN_1 {
+
+    
+    public static void main(String[] args) {
+     
+        
+       System.out.println("Hola");
+        
+    }
+    
+}
